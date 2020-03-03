8e72e79bfcea9ecdf8347a509c5a2f5c80164412
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-03-03 15:21:42 +0100

    Adding a data class to be used by the linkedlist sample.

---
+++ computer-science-tech-qas/vicd.app/DataStructures/LinkedList/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.LinkedList
+{
+    internal class Person
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Person(int id, string firstName, string lastName) =>
+            (Id, FirstName, LastName) = (id, firstName, lastName);
+    }
+}
