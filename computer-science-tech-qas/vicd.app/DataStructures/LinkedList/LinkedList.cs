811f1ea4591306e8a9dfeb7dcddc09e739946b34
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-03-03 15:26:57 +0100

    Adding node and linkedlist classes.

---
+++ computer-science-tech-qas/vicd.app/DataStructures/LinkedList/LinkedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.LinkedList
+{
+    internal class LinkedList
+    {
+        public Node HeadNode { get; private set; }
+
+        public LinkedList(Node headNode) =>
+            (HeadNode) = (headNode);
+    }
+}
---
+++ computer-science-tech-qas/vicd.app/DataStructures/LinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.LinkedList
+{
+    internal class Node
+    {
+        public Person PersonData { get; private set; }
+        public Node NextNode { get; private set; }
+
+        public Node(Person personData) =>
+            (PersonData) = (personData);
+
+        public void AssignNextNode(Node nextNode) =>
+            (NextNode) = (nextNode);
+    }
+}
