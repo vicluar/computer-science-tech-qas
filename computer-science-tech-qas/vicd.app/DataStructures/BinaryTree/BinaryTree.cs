c1adbc687c8e50e38a4fe00f7701a90a02e79bc9
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-03-09 16:46:26 +0100

    Adding base classes for BinaryTree sample.

---
+++ computer-science-tech-qas/vicd.app/DataStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.BinaryTree
+{
+    internal class BinaryTree
+    {
+    }
+}
---
+++ computer-science-tech-qas/vicd.app/DataStructures/BinaryTree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.BinaryTree
+{
+    internal class Node
+    {
+        public Pet PetData { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+
+        public Node(Pet petData) =>
+            (PetData) = (petData);
+
+        public void AddRightNode(Node node) =>
+            (RightChild) = (node);
+
+        public void AddLeftNode(Node node) =>
+            (LeftChild) = (node);
+    }
+}
