9674d53ef8652da31e54b8134a04bb6bc627c211
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-03-10 17:28:55 +0100

    Adding a node class for BST.

---
+++ computer-science-tech-qas/vicd.app/DataStructures/BinarySearchTree/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.BinarySearchTree
+{
+    internal class Node
+    {
+        public int Key { get; private set; }
+        public Node LeftNode { get; private set; }
+        public Node RigthNode { get; private set; }
+
+        public Node(int key) =>
+            (Key) = (key);
+    }
+}
