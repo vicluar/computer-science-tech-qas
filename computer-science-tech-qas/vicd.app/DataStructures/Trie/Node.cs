877801dce20dc2ed5d91875c930a46c3789d01bc
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-04-11 16:53:55 +0200

    Creating classes for Trie data structure.

---
+++ computer-science-tech-qas/vicd.app/DataStructures/Trie/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.Trie
+{
+    public class Node
+    {
+        private const int ALPHABET_SIZE = 26;
+
+        public Node[] ChildNodes { get; private set; }
+        public bool IsEndOfWord { get; private set; }
+
+        public Node()
+        {
+            ChildNodes = new Node[ALPHABET_SIZE];
+            IsEndOfWord = false;
+        }
+
+        public void SetAsEndOfWord()
+        {
+            IsEndOfWord = true;
+        }
+    }
+}
---
+++ computer-science-tech-qas/vicd.app/DataStructures/Trie/Trie.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vicd.app.DataStructures.Trie
+{
+    public class Trie
+    {
+        private Node _rootNode;
+
+        public Trie()
+        {
+            _rootNode = new Node();
+        }
+
+        public void Insert(string keyWord)
+        {
+            var currentNode = _rootNode;
+
+            for (int index = 0; index < keyWord.Length; index++)
+            {
+                var trieIndex = keyWord[index] - 'a';
+
+                if (currentNode.ChildNodes[trieIndex] == null)
+                    currentNode.ChildNodes[trieIndex] = new Node();
+
+                currentNode = currentNode.ChildNodes[trieIndex];
+            }
+
+            currentNode.SetAsEndOfWord();
+        }
+
+        public bool Search(string keyWorkd)
+        {
+            var currentNode = _rootNode;
+
+            for (int index = 0; index < keyWorkd.Length; index++)
+            {
+                var trieIndex = keyWorkd[index] - 'a';
+
+                if (currentNode.ChildNodes[trieIndex] == null)
+                    return false;
+
+                currentNode = currentNode.ChildNodes[trieIndex];
+            }
+
+            return currentNode != null && currentNode.IsEndOfWord;
+        }
+    }
+}
