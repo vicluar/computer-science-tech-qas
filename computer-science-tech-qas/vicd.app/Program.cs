b51c3c125f3e22695c7749163127ca96e6665457
Vicluar Daniel Merchan Briceño <vicluar@gmail.com>
2020-06-28 18:01:17 +0200

    Cleaning up old data structure files. Restarting from scratch.

--- computer-science-tech-qas/vicd.app/Program.cs
+++ computer-science-tech-qas/vicd.app/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using vicd.app.DataStructures.BinaryTree;
-using vicd.app.DataStructures.LinkedList;
-using vicd.app.DataStructures.Trie;
 
 namespace vicd.app
 {
@@ -10,33 +6,7 @@ namespace vicd.app
     {
         static void Main(string[] args)
         {
-            ExecuteTrieAutocompleteService();
-        }
-
-        private static void ExecuteTrieAutocompleteService()
-        {
-            Console.WriteLine("Type your word...");
-            var keyWord = Console.ReadLine();
-
-            var trieAutocompleteService = new TrieAutocompleteService();
-            var suggestedWords = trieAutocompleteService.GetSuggestedWords(keyWord);
-
-            if (suggestedWords == null || suggestedWords.Count <= 0)
-            {
-                Console.WriteLine("No suggested words");
-                return;
-            }
-                
-            PrintSuggestedWords(suggestedWords);
-        }
-
-        private static void PrintSuggestedWords(List<string> suggestedWords)
-        {
-            Console.WriteLine("Suggested words...");
-            foreach (var word in suggestedWords)
-            {
-                Console.WriteLine(word);
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
